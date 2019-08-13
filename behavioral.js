d31b29336da570c8cbc5685daff536f42f5f3062
Maryam Heydarian <maryamhdr76@gmail.com>
2019-08-13 20:19:53 +0430

    TypeError has fixed

--- behavioral.js
+++ behavioral.js
@@ -1,30 +1,12 @@
-window.onload = function () {
-    document.getElementById("input").value = "";
-    document.getElementById("list").innerHTML = "Nothing to do"
-}
-
-
 function addItem() {
-
-    var inputText = "";
 
     var inputField = document.getElementById("input");
     var listElement = document.getElementById("list");
 
-    if (listElement.innerHTML === "Nothing to do") {
-        listElement.innerHTML = ""
-    }
+    if (!inputField.value) return;
 
-    inputText = inputField.value;
-    inputField.value = "";
-
-    if (inputText.length === 0) {
-        alert("Please fill the input field.")
-    } else {
-        let element = document.createElement("li");
-        element.innerHTML = "<i class='far fa-list-alt fa-fw'></i>  " + inputText;
-        listElement.appendChild(element)
-        inputText = "";
-    }
-
+    var element = document.createElement("li");
+    element.innerHTML = inputField.value;
+    listElement.appendChild(element);
+    inputField.value = ""
 }
