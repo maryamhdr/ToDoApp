33fc0ca6f7bb3bb0e2174192ef919489d94b2172
maryamhdr <maryamhdr76@gmail.com>
2019-09-11 17:57:28 +0430

    Color pallete ghanged And tabs added.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var inputField = document.getElementById("input");
-var listElement = document.getElementById("list");
 
-function addItem() {
 
-    if (!inputField.value) return;
+// function addItem() {
 
-    var element = document.createElement("li");
-    element.innerHTML = inputField.value;
-    listElement.appendChild(element);
-    inputField.value = ""
+//     if (!inputField.value) return;
+
+//     var element = document.createElement("li");
+//     element.innerHTML = inputField.value;
+//     listElement.appendChild(element);
+//     inputField.value = ""
+// }
+
+function changeTab(evt, keyMode) {
+    var i, tabcontent, tablinks;
+
+    // tabcontent = document.getElementsByClassName("main-btn-container");
+    // for (i = 0; i < tabcontent.length; i++) {
+    //     tabcontent[i].style.display = "none";
+    // }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" tablinks-active", "");
+    }
+
+    // document.getElementById(keyMode).style.display = "flex";
+    evt.currentTarget.className += " tablinks-active";
 }
 
